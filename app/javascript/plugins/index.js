022df9a64445bef0ec19e3adbc7487ad165aacd3
Stephane Lafontaine <stephane@medqigong.ca>
2020-08-19 01:01:22 -0400

    Split select2 in 2 functions for placholders

--- app/javascript/plugins/index.js
+++ app/javascript/plugins/index.js
@@ -1,11 +1,12 @@
 import { initQuagga } from './init_quagga';
 import { initializeZxing } from './init_zxing';
 import { scanditTest } from './init_scandit';
-import { initSelect2 } from './init_select2';
+import { initSelect2, initSelect2Brew } from './init_select2';
 
 // initQuagga();
 // initializeZxing();
 document.addEventListener('turbolinks:load', () => {
   scanditTest();
   initSelect2();
+  initSelect2Brew();
 });
--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -5,17 +5,33 @@ const initSelect2 = () => {
   const url = window.location.href
   const newBeerRegex = /beers\/new/
   const newBeerTabRegex = /beer_tabs\/new/
+  const editBeerTabRegex = /beer_tabs\/\d+\/edit/
 
   $('.select2').select2({
     width: '100%',
-    templateSelection: function () {
-        if (newBeerRegex.test(url)) { // adjust for custom placeholder values
-          return 'Brewery name?';
-        } else if (newBeerTabRegex.test(url)) {
-          return 'How many ⭐️?';
-        }
-      }
+    placeholder: 'How many ⭐️?',
+    // templateSelection: function () {
+    //     if (newBeerRegex.test(url)) { // adjust for custom placeholder values
+    //       return 'Brewery name?';
+    //     } else if (newBeerTabRegex.test(url) || editBeerTabRegex.test(url)) {
+    //       return 'How many ⭐️?';
+    //     }
+    //   // return data.text;
+    //   },
+    // selectOnClose: true
   });
 };
 
-export { initSelect2 };
+const initSelect2Brew = () => {
+  const url = window.location.href
+  const newBeerRegex = /beers\/new/
+  const newBeerTabRegex = /beer_tabs\/new/
+  const editBeerTabRegex = /beer_tabs\/\d+\/edit/
+
+  $('.select2-breweries').select2({
+    width: '100%',
+    placeholder: 'Brewery name?',
+  });
+};
+
+export { initSelect2, initSelect2Brew };
